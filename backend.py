3b1050950a43490b46ab02f34751b5b18e3419b7
Shreya <shreyanakum2@gmail.com>
2024-12-12 13:06:22 -0800

    addditions plus minor bank change

--- backend.py
+++ backend.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-IMAGE_FOLDER = os.path.join(BASE_DIR, 'image-bank')#r"C:\Users\droso\Documents\Classifier-Site\image-bank"
+IMAGE_FOLDER = r"C:\Users\droso\Documents\Image-Splitting\2 21\C pops" #os.path.join(BASE_DIR, 'image-bank')#r"C:\Users\droso\Documents\Classifier-Site\image-bank"
 
 @app.route('/')
 def index():
@@ -18,7 +18,7 @@ def static_files(filename):
     return send_from_directory(os.path.join(os.path.dirname(__file__), 'static'), filename)
 
 def get_next_image():
-    images = [f for f in os.listdir(IMAGE_FOLDER) if f.endswith('.png') and not f.startswith(('p', 'n', 's'))]
+    images = [f for f in os.listdir(IMAGE_FOLDER) if f.endswith('.jpg') and not f.startswith(('p', 'n', 's'))]
     return random.choice(images) if images else None
 
 @app.route('/next-image', methods=['GET'])
